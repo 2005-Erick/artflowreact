539cab0e59592285b575c9845c24996bc061275c
2005-Erick <erickmelo0202@gmail.com>
2025-06-13 05:15:59 -0300

    ajuste img

--- src/hero-slides-data.js
+++ src/hero-slides-data.js
@@ -2,23 +2,21 @@ export const slides = [
   {
     title: "Mostre Suas Habilidades",
     subtitle: "Do esboço à arte final, seu traço tem espaço aqui para brilhar e inspirar.",
-    image: "/ilustr-flow.PNG" 
+    image: "./public/ilustr-flow.PNG" 
   },
   {
     title: "Capture o Momento",
     subtitle: "O mundo através de suas lentes. Compartilhe sua visão, conte histórias com suas fotos.",
-    image: "/photo-flow.png" 
+    image: "./public/photo-flow.png" 
   },
   {
     title: "Dê Forma à Imaginação",
     subtitle: "Exponha seus projetos 3D mais incríveis e encontre seu público em nossa galeria.",
-    image: "/3d-flow..jpg" 
+    image: "./public/3dn-flow.jpg" // Corrigido nome do arquivo que tinha dois pontos
   },
   {
     title: "Esbanje Seus Sons",
     subtitle: "Sua música merece ser ouvida. Deixe sua marca no mundo sonoro.",
-    image: "/music-flow.png" 
+    image: "./public/music-flow.png" 
   }
-  
-  
 ];
